01d79158189bdd86542c55a523f130792d1b58e3
ahmedhussain47 <68425053+ahmedhussain47@users.noreply.github.com>
2022-05-26 00:21:29 +0500

    js1

---
+++ app.js
@@ -0,0 +1,52 @@
+// Chapter 1
+alert("Chapter 1")
+
+// Question 1 & 2
+
+alert("Error! Please enter a valid password");
+
+// Question 3
+alert("Welcome to JS Land...\nHappy Coding");
+
+// Question 4
+alert("Welcome to JS Land :)")
+alert("Happy Coding!")
+
+// Question 5
+alert("Hello.. I can run JS through my web browser's console")
+
+// Chapter 2
+
+alert("Chapter 2")
+
+// Question 1,2,3 & 4
+
+var name = "ahmed";
+alert(name)
+
+var name = "23 Year Old";
+alert(name);
+
+var name ="Certified Mobile Application Development!"
+alert(name)
+
+// Question 5
+alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+
+// Question 6
+
+var emailaddress = "ahmedraza@gmail.com"
+alert("My Email Address is : " + emailaddress);
+
+// Question 7
+var book = "A Smarter\nway to learn JavaScript"
+alert("I'm trying to learn from the book "+ book);
+ 
+// Question 8
+
+document.write("Yah! I can write HTML content through JavaScript")
+
+// Question 9
+
+var pattern = "~~~~~~~~~~:)~~~~~~~~~~" ;
+alert(pattern);
